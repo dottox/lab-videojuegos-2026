ff3c10cca4818e27717fe736f723201632882342
dottox <imsm2424@gmail.com>
2026-04-12 01:32:04 -0300

    feat: game configs

---
+++ src/configs/gameplay_config.lua
@@ -0,0 +1,13 @@
+GAMEPLAY_CONFIG = {
+    -- player game zone, restricts movement
+    GAME_ZONE = { x=16, y=20, w=100, h=80 },
+    
+    PROJECTILE_TYPES = {
+        BULLET = 1,
+        LASER = 2,
+    },
+
+    -- background patches that changes mechanics of entities
+    PATCH_TYPES = {
+    }
+}
