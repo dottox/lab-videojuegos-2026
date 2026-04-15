a18c8a78b3f4a9b83e88e3a2fc73ec27812d9567
dottox <imsm2424@gmail.com>
2026-04-14 21:23:32 -0300

    chore: added player to main and removed various logs

--- src/main.lua
+++ src/main.lua
@@ -4,15 +4,17 @@
 
 #include configs/game_config.lua
 #include configs/gameplay_config.lua
+#include levels/level.lua
 #include levels/levels_data.lua
 #include entities/pattern.lua
 #include entities/projectile.lua
+#include entities/player.lua
 #include utility/hex_utils.lua
+#include utility/logger.lua
 
 SCALE_MS = 10
 
 function _init()
-    game_time_ms = 0
     current_level = 1
     
     -- Load and decode level
@@ -20,6 +22,7 @@ function _init()
     
     -- Projectile system
     active_projectiles = {}
+    player = create_player(64, 64, 100)
     next_spawn_idx = 1
     
     -- Debug
@@ -27,8 +30,6 @@ function _init()
 end
 
 function _update()
-    game_time_ms += 16.67
-    
     -- Spawn new projectiles based on timing
     spawn_projectiles()
     
@@ -39,52 +40,19 @@ end
 function _draw()
     cls(0)
     
-    -- Draw time
-    printh("time: "..(game_time_ms/1000).."s", 2, 2, 7)
-    printh("spawned: "..spawned_count, 2, 10, 7)
-    printh("active: "..#active_projectiles, 2, 18, 7)
-    
     -- Draw active projectiles
     draw_projectiles()
+
+    draw_player()
+
+    move_player()
+
+    draw_logger_hud()
     
     -- Draw level info
-    printh("level: "..current_level, 2, 110, 7)
 end
 
 -- ============================================
 -- LEVEL LOADING
 -- ============================================
 
-function load_level(level_id)
-    level_data = LEVEL_DATA[level_id]
-    
-    if not level_data then
-        printh("ERROR: Level "..level_id.." not found!")
-        return
-    end
-    
-    -- Decode patterns
-    patterns = decode_patterns(level_data.patterns)
-    
-    -- Decode projectiles
-    projectiles_to_spawn = decode_projectiles(level_data.projectiles, patterns)
-    
-    printh("Level loaded with "..#projectiles_to_spawn.." projectiles")
-end
-
--- ============================================
--- UTILITY
--- ============================================
-
-function add(t, item)
-    t[#t + 1] = item
-end
-
-function del(t, item)
-    for i = #t, 1, -1 do
-        if t[i] == item then
-            table.remove(t, i)
-            break
-        end
-    end
-end
